b83852ac4a0fc3041c0de5c281cfa82b8643e56b
Anderson <andersonmoal06@gmail.com>
2026-01-19 20:19:16 -0600

    Correción formato de impresión no requerido por instrucciones

--- MayusculasMinusculas.java
+++ MayusculasMinusculas.java
@@ -7,7 +7,7 @@ public class MayusculasMinusculas {
         System.out.println("Escriba una frase");
         String frase = scanner.nextLine();
 
-        System.out.printf("Frase en mayúsculas: %s%nFrase en minúsculas: %s%n", frase.toUpperCase(),
-                frase.toLowerCase());
+        System.out.println("Frase original: " + frase + "\nFrase en mayúsculas: " + frase.toUpperCase()
+                + "\nFrase en minúsculas: " + frase.toLowerCase());
     }
 }
